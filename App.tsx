20e754c5208ac5b7a69ba8629cdca652311838ee
shridharbavannawar <shridhar.b@razorpay.com>
2021-12-28 15:27:40 +0530

    refactor: expose mpos wrapper methods

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -19,7 +19,12 @@ import {
   TouchableHighlight,
   Alert,
 } from 'react-native';
-import { init } from 'react-native-mpos-wrapper';
+import {
+  init,
+  initializeTransaction,
+  refreshToken,
+  MposWrapperEmitter,
+} from 'react-native-mpos-wrapper';
 
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
@@ -29,6 +34,19 @@ const App = () => {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+
+  useEffect(() => {
+    console.log('Registering the events...');
+    MposWrapperEmitter?.addListener('RefreshToken', data => {
+      console.log('refreshToken Listener:', JSON.stringify(data));
+    });
+    MposWrapperEmitter?.addListener('TransactionResult', data => {
+      console.log('TransactionResult Listener:', JSON.stringify(data));
+    });
+    MposWrapperEmitter?.addListener('TransactionUIEvent', data => {
+      console.log('TransactionUIEvent Listener:', JSON.stringify(data));
+    });
+  }, []);
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -50,7 +68,6 @@ const App = () => {
           <TouchableHighlight
             style={{ padding: 28, backgroundColor: 'aqua', borderRadius: 12 }}
             onPress={async () => {
-              //const result = await multiply(2, 3);
               try {
                 const u = await init();
                 Alert.alert(`${u}`);
@@ -59,6 +76,48 @@ const App = () => {
               }
             }}>
             <Text>Click to Initialize MPOS SDK</Text>
+          </TouchableHighlight>
+
+          <TouchableHighlight
+            style={{
+              padding: 28,
+              marginTop: 24,
+              backgroundColor: 'pink',
+              borderRadius: 12,
+            }}
+            onPress={async () => {
+              try {
+                await refreshToken((d, a) => {
+                  console.log('refreshToken callback', d);
+                });
+                //Alert.alert(`${u}`);
+              } catch (e) {
+                console.log('Error', e);
+                Alert.alert('Error in transacting');
+              }
+            }}>
+            <Text>Refresh Token</Text>
+          </TouchableHighlight>
+
+          <TouchableHighlight
+            style={{
+              padding: 28,
+              marginTop: 24,
+              backgroundColor: 'yellow',
+              borderRadius: 12,
+            }}
+            onPress={async () => {
+              try {
+                await initializeTransaction((d, a) => {
+                  console.log('initializeTransaction callback', d);
+                });
+                //Alert.alert(`${u}`);
+              } catch (e) {
+                console.log('Error', e);
+                Alert.alert('Error in transacting');
+              }
+            }}>
+            <Text>Transact Rs 100</Text>
           </TouchableHighlight>
         </View>
       </ScrollView>
